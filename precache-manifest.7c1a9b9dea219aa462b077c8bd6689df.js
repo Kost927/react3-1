9a6c86107f961fdaeeab837879aef3868d66aa70
Kostya Polishko <kost927@gmail.com>
2020-08-19 21:43:06 +0300

    Updates

---
+++ precache-manifest.7c1a9b9dea219aa462b077c8bd6689df.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2276fd7033955500a1edac5e4678d013",
+    "url": "/react3-1/index.html"
+  },
+  {
+    "revision": "a12668c4f34965649801",
+    "url": "/react3-1/static/css/main.cb1bb0fb.chunk.css"
+  },
+  {
+    "revision": "5c7247bac74b9f6c20bf",
+    "url": "/react3-1/static/js/2.84f681a4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react3-1/static/js/2.84f681a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a12668c4f34965649801",
+    "url": "/react3-1/static/js/main.3e49832d.chunk.js"
+  },
+  {
+    "revision": "ae7279454552c3d2383e",
+    "url": "/react3-1/static/js/runtime-main.862881b0.js"
+  }
+]);
